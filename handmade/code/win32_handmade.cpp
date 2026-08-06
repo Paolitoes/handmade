3eb3b474281e69b46265016e4d99ffc002436948
Paolitoes <carlospaolo15@gmail.com>
2026-08-05 23:51:03 -0400

    Initial commit

---
+++ handmade/code/win32_handmade.cpp
@@ -0,0 +1,98 @@
+
+#include <windows.h>
+
+LRESULT CALLBACK
+MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+{
+    LRESULT Result = 0;
+    
+    switch(Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+        
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n"); 
+        } break; 
+
+        default:
+        {
+            //OutputDebugStringA("default\n"); 
+            Result = DefWindowProcW(Window, Message, WParam, LParam);
+        } break;
+    }
+
+    return(Result);
+}
+
+int CALLBACK 
+WinMain(HINSTANCE Instance, 
+        HINSTANCE PrevInstance, 
+        LPSTR CommandLine, 
+        int ShowCode)
+{
+    WNDCLASS WindowClass = {}; 
+
+    WindowClass.style = CS_OWNDC|CS_VREDRAW|CS_HREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = Instance;
+    //WindowClass.hIcon = ;
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+    if(RegisterClass(&WindowClass))
+    {
+        HWND WindowHandle = 
+            CreateWindowEx(
+                0,
+		WindowClass.lpszClassName,
+		"Handmade Hero",
+		WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		0,
+		0,
+		Instance,
+		0);  
+        if(WindowHandle)
+        {
+            for(;;)
+            {
+                MSG Message;
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if(MessageResult >0)
+                {
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+        }
+        
+    }
+    else
+    {
+    }
+
+    return(0);
+}
---
+++ handmade/misc/shell.bat
@@ -0,0 +1,4 @@
+@echo off
+title Handmade Shell
+set PATH=%PATH%;w:\handmade\misc
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
